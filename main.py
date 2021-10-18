8177fddfa4163376cbb83e41eb65f6385bf08862
Ardcy <zhjgtao@gmail.com>
2021-10-18 13:44:31 +0800

    upload main.py

--- main.py
+++ main.py
@@ -11,11 +11,35 @@ import torchvision.transforms as transforms
 import os
 import sys
 import time
+import numpy as np
+import random
+import datetime
+import argparse
+
+def setup_seed(seed):
+     torch.manual_seed(seed)
+     torch.cuda.manual_seed_all(seed)
+     np.random.seed(seed)
+     random.seed(seed)
+     torch.backends.cudnn.deterministic = True
+
+
 _, term_width = os.popen('stty size', 'r').read().split()
 term_width = int(term_width)
 TOTAL_BAR_LENGTH = 65.
 last_time = time.time()
 begin_time = last_time
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+parser.add_argument('--resume', '-r', action='store_true',
+                    help='resume from checkpoint')
+parser.add_argument('--epoch', default=200,type=int,help='epoch num')
+parser.add_argument('--batch_size', default=256,type=int,help='batch_size for train datasets')
+
+args = parser.parse_args()
+
+
 def progress_bar(current, total, msg=None):
     global last_time, begin_time
     if current == 0:
@@ -58,7 +82,8 @@ def progress_bar(current, total, msg=None):
     else:
         sys.stdout.write('\n')
     sys.stdout.flush()
-    
+
+
 def format_time(seconds):
     days = int(seconds / 3600/24)
     seconds = seconds - days*3600*24
@@ -90,18 +115,10 @@ def format_time(seconds):
     if f == '':
         f = '0ms'
     return f
-import datetime
+
+
 starttime = datetime.datetime.now()
-import numpy as np
-import random
-def setup_seed(seed):
-     torch.manual_seed(seed)
-     torch.cuda.manual_seed_all(seed)
-     np.random.seed(seed)
-     random.seed(seed)
-     torch.backends.cudnn.deterministic = True
-# 设置随机数种子
-setup_seed(20)
+
 
 class BasicBlock(nn.Module):
     expansion = 1
@@ -199,66 +216,12 @@ class ResNet(nn.Module):
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
-learningrate = 0.1
-
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-best_acc = 0  # best test accuracy
-start_epoch = 0  # start from epoch 0 or last checkpoint epoch
-
-# Data
-print('==> Preparing data..')
-transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4), # 随机裁剪
-    transforms.RandomHorizontalFlip(), # 依概率水平翻转
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-transform_test = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
-])
-
-trainset = torchvision.datasets.CIFAR10(
-    root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(
-    trainset, batch_size=128, shuffle=True, num_workers=2)
-
-testset = torchvision.datasets.CIFAR10(
-    root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(
-    testset, batch_size=100, shuffle=False, num_workers=2)
 
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
 
-# Model
-print('==> Building model..')
-
-net = ResNet18()
-
-net = net.to(device)
-if device == 'cuda':
-    net = torch.nn.DataParallel(net)
-    cudnn.benchmark = True
-
-if 0:
-    # Load checkpoint.
-    print('==> Resuming from checkpoint..')
-    assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
-    checkpoint = torch.load('./checkpoint/ckpt.pth')
-    net.load_state_dict(checkpoint['net'])
-    best_acc = checkpoint['acc']
-    start_epoch = checkpoint['epoch']
-
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr=learningrate,
-                      momentum=0.9, weight_decay=5e-4)
-scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
-
-result = []
+result = [[], [], [], [], []]  # epoch,测试 acc 测试loss, 训练 acc 训练集 loss
 # Training
+
 def train(epoch):
     print('\nEpoch: %d' % epoch)
     net.train()
@@ -281,34 +244,39 @@ def train(epoch):
         progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                      % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
 
+    result[3].append(100.*correct/total)
+    result[4].append((train_loss/(batch_idx+1)))
 
 def test(epoch):
     global best_acc
     global result
     net.eval()
-    test_loss = 0
-    correct = 0
-    total = 0
+    test_loss=0
+    correct=0
+    total=0
     with torch.no_grad():
         for batch_idx, (inputs, targets) in enumerate(testloader):
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+            inputs, targets=inputs.to(device), targets.to(device)
+            outputs=net(inputs)
+            loss=criterion(outputs, targets)
 
             test_loss += loss.item()
-            _, predicted = outputs.max(1)
+            _, predicted=outputs.max(1)
             total += targets.size(0)
             correct += predicted.eq(targets).sum().item()
 
-           
+
             progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
                          % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
     # Save checkpoint.
-    acc = 100.*correct/total
-    result.append((epoch,acc))
+    acc=100.*correct/total
+    result[0].append(epoch)
+    result[1].append(acc)
+    result[2].append((test_loss/(batch_idx+1)))
+
     if acc > best_acc:
         print('Saving..')
-        state = {
+        state={
             'net': net.state_dict(),
             'acc': acc,
             'epoch': epoch,
@@ -316,18 +284,81 @@ def test(epoch):
         if not os.path.isdir('checkpoint'):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
-        best_acc = acc
+        best_acc=acc
 
 
-for epoch in range(start_epoch, start_epoch+1):
-    train(epoch)
-    test(epoch)
-    scheduler.step()
-    
-endtime = datetime.datetime.now()
-print("训练结束，耗时 %d s" % (endtime - starttime).seconds)
+if __name__ == '__main__':
 
-state = {
-    'result':result,
-}
-torch.save(state,'./checkpoint/result.pth')
+    setup_seed(20)
+          
+    device='cuda' if torch.cuda.is_available() else 'cpu'
+    best_acc=0  # best test accuracy
+    start_epoch=0  # start from epoch 0 or last checkpoint epoch
+
+    # Data
+    print('==> Preparing data..')
+    transform_train=transforms.Compose([
+        transforms.RandomCrop(32, padding=4),  # 随机裁剪
+        transforms.RandomHorizontalFlip(),  # 依概率水平翻转
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465),
+                             (0.2023, 0.1994, 0.2010)),
+    ])
+
+    trainset=torchvision.datasets.CIFAR10(
+        root='./data', train=True, download=True, transform=transform_train)
+    trainloader=torch.utils.data.DataLoader(
+        trainset, batch_size=args.batch_size, shuffle=True, num_workers=2)
+
+    testset=torchvision.datasets.CIFAR10(
+        root='./data', train=False, download=True, transform=transform_test)
+    testloader=torch.utils.data.DataLoader(
+        testset, batch_size=100, shuffle=False, num_workers=2)
+
+
+    classes=('plane', 'car', 'bird', 'cat', 'deer',
+            'dog', 'frog', 'horse', 'ship', 'truck')
+
+    # Model
+    print('==> Building model..')
+
+    net=ResNet18()
+
+    net=net.to(device)
+    if device == 'cuda':
+        net=torch.nn.DataParallel(net)
+        cudnn.benchmark=True
+
+    if args.resume:
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        assert os.path.isdir(
+            'checkpoint'), 'Error: no checkpoint directory found!'
+        checkpoint=torch.load('./checkpoint/ckpt.pth')
+        net.load_state_dict(checkpoint['net'])
+        best_acc=checkpoint['acc']
+        start_epoch=checkpoint['epoch']
+
+    criterion=nn.CrossEntropyLoss()
+    optimizer=optim.SGD(net.parameters(), lr=args.lr,
+                        momentum=0.9, weight_decay=5e-4)
+    scheduler=torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+
+    for epoch in range(start_epoch, start_epoch+args.epoch):
+        train(epoch)
+        test(epoch)
+        scheduler.step()
+
+    endtime=datetime.datetime.now()
+    print("训练结束，耗时 %d s" % (endtime - starttime).seconds)
+
+    acc_result={
+        'result': result,
+    }
+    torch.save(acc_result, './checkpoint/result.pth')
